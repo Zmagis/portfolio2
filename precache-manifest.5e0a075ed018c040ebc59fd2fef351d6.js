19e536e11ee5b5c65671b41f061e41eac19878e1
Zmagis <eglegustaite@gmail.com>
2020-09-07 00:02:31 +0300

    Updates

---
+++ precache-manifest.5e0a075ed018c040ebc59fd2fef351d6.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "903ce9699d47d4bfd849806c28f48e92",
+    "url": "/portfolio2/index.html"
+  },
+  {
+    "revision": "de91c65f5999bf7ab7e9",
+    "url": "/portfolio2/static/css/main.2f01afb6.chunk.css"
+  },
+  {
+    "revision": "695116696cfac6599915",
+    "url": "/portfolio2/static/js/2.45bac368.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/portfolio2/static/js/2.45bac368.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de91c65f5999bf7ab7e9",
+    "url": "/portfolio2/static/js/main.364d5f0d.chunk.js"
+  },
+  {
+    "revision": "dcd25c0e364e68ac0ac8",
+    "url": "/portfolio2/static/js/runtime-main.1842d288.js"
+  },
+  {
+    "revision": "89f0ab5c462d94476c36ddbeeb4f7490",
+    "url": "/portfolio2/static/media/TheHike.89f0ab5c.png"
+  },
+  {
+    "revision": "3c28c740a3210a6197d712e1cf492788",
+    "url": "/portfolio2/static/media/css.3c28c740.png"
+  },
+  {
+    "revision": "42f29c71c3a7a6507c9110fd15f9e7e7",
+    "url": "/portfolio2/static/media/form.42f29c71.png"
+  },
+  {
+    "revision": "21a2c59aac67f78c027e2e914c8d8453",
+    "url": "/portfolio2/static/media/home.21a2c59a.jpg"
+  },
+  {
+    "revision": "666c190e1f7a462852f10506d3375b5d",
+    "url": "/portfolio2/static/media/html.666c190e.png"
+  },
+  {
+    "revision": "7cbd6932e100d94ebb069aa6f9dcaeb5",
+    "url": "/portfolio2/static/media/images.7cbd6932.png"
+  },
+  {
+    "revision": "0ea56a898919225f945ac241171392ec",
+    "url": "/portfolio2/static/media/infinite.0ea56a89.png"
+  },
+  {
+    "revision": "7f5403ccd080015b83bc1f40c1881343",
+    "url": "/portfolio2/static/media/java.7f5403cc.png"
+  },
+  {
+    "revision": "c647884d2940f119b665a453f9c2c8f3",
+    "url": "/portfolio2/static/media/js.c647884d.png"
+  },
+  {
+    "revision": "1e729d069321bc863adbe29b86afa13b",
+    "url": "/portfolio2/static/media/leon2.1e729d06.png"
+  },
+  {
+    "revision": "41bd2bee267f633af35ab9a348c08a27",
+    "url": "/portfolio2/static/media/me.41bd2bee.jpg"
+  },
+  {
+    "revision": "c502658a509d27b53679b3ef73c0d82f",
+    "url": "/portfolio2/static/media/nodejs.c502658a.png"
+  },
+  {
+    "revision": "0e4b3a8b9cc077422febd7afec900546",
+    "url": "/portfolio2/static/media/parallax2.0e4b3a8b.jpg"
+  },
+  {
+    "revision": "f32d240822971eb413c93bf961500d03",
+    "url": "/portfolio2/static/media/python.f32d2408.png"
+  },
+  {
+    "revision": "37fe8322b169ddbdeabf75930e886ac6",
+    "url": "/portfolio2/static/media/react.37fe8322.png"
+  },
+  {
+    "revision": "fd9ab9ee672ba92de7d809b53dbb6bdd",
+    "url": "/portfolio2/static/media/redux.fd9ab9ee.png"
+  },
+  {
+    "revision": "09fa297368663ab9063c785331427a40",
+    "url": "/portfolio2/static/media/sass.09fa2973.png"
+  },
+  {
+    "revision": "dc2d682ffd09e52946ee44e16fb891f4",
+    "url": "/portfolio2/static/media/shop.dc2d682f.png"
+  },
+  {
+    "revision": "40843fe8aea84c21489f8a616319d48c",
+    "url": "/portfolio2/static/media/sql.40843fe8.png"
+  }
+]);
