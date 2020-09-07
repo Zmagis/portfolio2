9bf999824d9ba5aaa1351c0e7cb4c7771a09f2b6
Zmagis <eglegustaite@gmail.com>
2020-09-07 10:24:44 +0300

    about section. change keywords color

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -17,17 +17,21 @@ const About = () => {
         <p>
           I chose to try programming, even though at the time I didn't even know
           what HTML was. The more time I spent learning to code, the harder I
-          found it was for me to leave my cozy place in front of my computer
-          screen. Without a doubt, programming is what brings me joy and
-          excitement to learn and create more every single day.
+          found it was for me to leave my{' '}
+          <span>cozy place in front of my computer screen</span>. Without a
+          doubt, programming is what brings me <span>joy and excitement</span>{' '}
+          to learn and create more every single day.
         </p>
         <p>
-          I took numerous online courses on platforms like Udemy, Scrimba,
-          FreeCodeCamp. I have successfully finished the European Digital
-          Bootcamp; I have also experienced what it is like to work with an
-          agile team on a real-life project, when I participated in the project
-          A Month with IT. And now my hunger for new challenges and real-life
-          projects is bigger than ever.
+          I took numerous online courses on platforms like{' '}
+          <span>Udemy, Scrimba, FreeCodeCamp</span>. I have successfully
+          finished the <span>European Digital Bootcamp</span>; I have also
+          experienced what it is like to work with
+          <span> an agile team on a real-life project</span>, when I
+          participated in the project
+          <span> A Month with IT</span>. And now my{' '}
+          <span>hunger for new challenges</span> and real-life projects is
+          bigger than ever.
         </p>
         {/* <p>
           In those rare occasions when I'm not in front of a computer screen,
